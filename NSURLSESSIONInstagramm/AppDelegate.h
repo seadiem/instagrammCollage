9d86262b9025b055b4ba5feb106484df12c304fb
mac <mac@Mac-mini-mac.local>
2015-05-04 17:34:36 +0300

    Initial Commit

---
+++ NSURLSESSIONInstagramm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSESSIONInstagramm
+//
+//  Created by mac on 04.05.15.
+//  Copyright (c) 2015 soft eng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSESSIONInstagramm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSESSIONInstagramm
+//
+//  Created by mac on 04.05.15.
+//  Copyright (c) 2015 soft eng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
